e48c7d6c73a90cb8274286524c3b14124521884a
Adriana <84536824+itsmeadriana@users.noreply.github.com>
2021-11-17 12:40:50 -0600

    home-routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,7 +3,9 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes.js');
 const commentRoutes = require('./comment-routes.js');
+const homeRoutes = require('./home-routes.js');
 
+router.use('/', homeRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
 router.use('/users', userRoutes);
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,6 +2,11 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
+router.get('/', (req,res) => {
+  res.render('homepage');
+});
+
+
 router.get('/', (req, res) => {
     Post.findAll({
         attributes: [
@@ -36,6 +41,10 @@ router.get('/', (req, res) => {
           console.log(err);
           res.status(500).json(err);
         });
+});
+
+router.get('/login', (req, res) => {
+  res.render('login');
 });
 
 module.exports = router;
